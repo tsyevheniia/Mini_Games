37ec93d10e01dd9c425a63934b6376fcdd3492ab
ts.yevheniia <ts.yevheniia@google.com>
2024-04-09 12:20:52 +0200

    Add 'Guess-Number' Game

---
+++ Guess-Number/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+//check-button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('⛔️ No Number!');
+  }
+
+  // when players win
+  else if (guess === secretNumber) {
+    displayMessage('🥳 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b327';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+
+  //when guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('☹️ You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  } else {
+    document.querySelector('.message').textContent = '☹️ You lost the game ';
+    document.querySelector('.score').textContent = 0;
+  }
+});
+
+//again-button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing... ');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
